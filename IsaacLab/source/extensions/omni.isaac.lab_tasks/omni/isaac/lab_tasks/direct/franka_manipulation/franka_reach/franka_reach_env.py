fe014146af92bc4cfaf8d88d4de67d24154842b0
jm <jmleeluck@snu.ac.kr>
2024-11-25 21:38:58 +0900

    update reward system same to metaworld

--- IsaacLab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_manipulation/franka_reach/franka_reach_env.py
+++ IsaacLab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_manipulation/franka_reach/franka_reach_env.py
@@ -1,6 +1,7 @@
 import torch
 from omni.isaac.lab.utils import configclass
 from ..franka_manipulation import FrankaBaseEnv, FrankaBaseEnvCfg
+from ..reward_utils.reward_utils import *
 
 @configclass
 class FrankaReachEnvCfg(FrankaBaseEnvCfg):
@@ -14,25 +15,17 @@ class FrankaReachEnv(FrankaBaseEnv):
         super().__init__(cfg, render_mode, **kwargs)
 
     def _get_rewards(self) -> torch.Tensor:
-        ee_pos = torch.mean(self.robot.data.body_state_w[:, self.finger_idx, 0:3], dim = 1)
+        tcp = torch.mean(self.robot.data.body_state_w[:, self.finger_idx, 0:3], dim = 1)
         self.update_target_pos()
-        dist = torch.norm(ee_pos - self.target_pos, dim=1)
-        dist_xy = torch.norm(ee_pos[:,:2] - self.target_pos[:,:2], dim = 1)
-        dist_z = torch.norm(ee_pos[:,-1] - self.target_pos[:,-1])
-        reward = torch.where(dist_xy<0.06, -dist, - dist_xy - dist_z)
-        reward += torch.where(dist <= 0.05, 50, 0)
+        tcp_to_target = torch.norm(tcp-self.target_pos, dim = 1)
+        in_place_margin = torch.norm(self.init_tcp - self.target_pos, dim = 1)
+        in_place = tolerance(
+            tcp_to_target,
+            bounds = (0, 0.05),
+            margin = in_place_margin,
+        )
 
-        wrist_pos = self.robot.data.body_state_w[:,self.ee_idx,0:3]
-        wrist_to_ee = ee_pos - wrist_pos
-        wrist_to_ee = torch.nn.functional.normalize(wrist_to_ee, dim = 1)
-
-        ee_to_target = self.target_pos - ee_pos
-        ee_to_target = torch.nn.functional.normalize(ee_to_target,dim = 1)
-
-        for env_id in range(self.num_envs):
-            reward[env_id] += 10*torch.dot(wrist_to_ee[env_id], ee_to_target[env_id])
-
-        return reward
+        return 10*in_place
 
     def _get_dones(self) -> tuple[torch.Tensor, torch.Tensor]:
         ee_pos = torch.mean(self.robot.data.body_state_w[:, self.finger_idx, 0:3], dim = 1)
@@ -42,7 +35,7 @@ class FrankaReachEnv(FrankaBaseEnv):
         undesired_contact_body_ids,_ = self.sensor.find_bodies(['panda_link0', 'panda_link1', 'panda_link2', 'panda_link3', 'panda_link4', 'panda_link5', 'panda_link6', 'panda_link7'])
         dones = torch.empty((self.num_envs,), dtype = torch.bool, device = self.device)
         for env_id in range(self.num_envs):
-            dones[env_id] = distances[env_id] <= 0.01 or distances[env_id] >= 1.5 or torch.any(torch.norm(self.sensor.data.net_forces_w[env_id, undesired_contact_body_ids, :], dim=-1) > 1e-3) or self.target_pos[env_id,-1] < 0.8
+            dones[env_id] = distances[env_id] <= 0.05 or distances[env_id] >= 1.5 or torch.any(torch.norm(self.sensor.data.net_forces_w[env_id, undesired_contact_body_ids, :], dim=-1) > 1e-3) or self.target_pos[env_id,-1] < 0.8
 
         time_out = self.episode_length_buf >= self.max_episode_length - 1
         return dones, time_out
