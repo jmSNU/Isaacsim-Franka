03c841685d16823296b8de91a474f1b65e952b68
jm <jmleeluck@snu.ac.kr>
2024-12-05 13:01:07 +0900

    add various sigmoid functions

--- IsaacLab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_manipulation/reward_utils/reward_utils.py
+++ IsaacLab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_manipulation/reward_utils/reward_utils.py
@@ -1,7 +1,63 @@
 import torch
-import numpy as  np
+import numpy as np
 
-def tolerance(x, bounds, margin, value_at_margin=0.1):
+def _sigmoid(x, value_at_1, sigmoid):
+    if sigmoid in ("cosine", "linear", "quadratic"):
+        if not 0 <= value_at_1 < 1:
+            raise ValueError(
+                f"`value_at_1` must be nonnegative and smaller than 1, got {value_at_1}."
+            )
+    else:
+        if not 0 < value_at_1 < 1:
+            raise ValueError(
+                f"`value_at_1` must be strictly between 0 and 1, got {value_at_1}."
+            )
+
+    if sigmoid == "gaussian":
+        scale = torch.sqrt(-2 * torch.log(torch.tensor(value_at_1, dtype=x.dtype)))
+        return torch.exp(-0.5 * (x * scale) ** 2)
+
+    elif sigmoid == "hyperbolic":
+        scale = torch.arccosh(1 / torch.tensor(value_at_1, dtype=x.dtype))
+        return 1 / torch.cosh(x * scale)
+
+    elif sigmoid == "long_tail":
+        scale = torch.sqrt(1 / torch.tensor(value_at_1, dtype=x.dtype) - 1)
+        return 1 / ((x * scale) ** 2 + 1)
+
+    elif sigmoid == "reciprocal":
+        scale = 1 / torch.tensor(value_at_1, dtype=x.dtype) - 1
+        return 1 / (torch.abs(x) * scale + 1)
+
+    elif sigmoid == "cosine":
+        scale = torch.arccos(2 * torch.tensor(value_at_1, dtype=x.dtype) - 1) / np.pi
+        scaled_x = x * scale
+        ret = torch.where(
+            torch.abs(scaled_x) < 1, (1 + torch.cos(np.pi * scaled_x)) / 2, 0.0
+        )
+        return ret.item() if isinstance(x, torch.Tensor) and x.numel() == 1 else ret
+
+    elif sigmoid == "linear":
+        scale = 1 - torch.tensor(value_at_1, dtype=x.dtype)
+        scaled_x = x * scale
+        ret = torch.where(torch.abs(scaled_x) < 1, 1 - scaled_x, 0.0)
+        return ret.item() if isinstance(x, torch.Tensor) and x.numel() == 1 else ret
+
+    elif sigmoid == "quadratic":
+        scale = torch.sqrt(1 - torch.tensor(value_at_1, dtype=x.dtype))
+        scaled_x = x * scale
+        ret = torch.where(torch.abs(scaled_x) < 1, 1 - scaled_x**2, 0.0)
+        return ret.item() if isinstance(x, torch.Tensor) and x.numel() == 1 else ret
+
+    elif sigmoid == "tanh_squared":
+        scale = torch.arctanh(torch.sqrt(1 - torch.tensor(value_at_1, dtype=x.dtype)))
+        return 1 - torch.tanh(x * scale) ** 2
+
+    else:
+        raise ValueError(f"Unknown sigmoid type {sigmoid!r}.")
+
+
+def tolerance(x, bounds, margin, sigmoid, value_at_margin=0.1):
     lower, upper = bounds
     assert lower < upper and torch.all(margin > 0)
     
@@ -13,8 +69,7 @@ def tolerance(x, bounds, margin, value_at_margin=0.1):
     else:
         # Compute distance d and apply sigmoid function
         d = torch.where(x < lower, lower - x, x - upper) / margin
-        sigmoid = lambda x, value_at_1: 1 / ((x * np.sqrt(1 / value_at_1 - 1))**2 + 1)
-        value = torch.where(in_bounds, 1.0, sigmoid(d, value_at_margin))
+        value = torch.where(in_bounds, 1.0, _sigmoid(d, value_at_margin, sigmoid))
 
     return value
 
