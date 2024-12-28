9de49dfd635fe933b5bdd04be611ebb89854361b
jm <jmleeluck@snu.ac.kr>
2024-12-28 22:39:55 +0900

    add tcp position into member variable of FrankaBaseEnv

--- IsaacLab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_manipulation/franka_reach/franka_reach_env.py
+++ IsaacLab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/franka_manipulation/franka_reach/franka_reach_env.py
@@ -15,9 +15,9 @@ class FrankaReachEnv(FrankaBaseEnv):
         super().__init__(cfg, render_mode, **kwargs)
 
     def _get_rewards(self) -> torch.Tensor:
-        tcp = torch.mean(self.robot.data.body_state_w[:, self.finger_idx, 0:3], dim = 1)
-        tcp_to_goal = torch.norm(tcp - self.goal, dim = 1)
-
+        self.compute_intermediate()
+        tcp_to_goal = torch.norm(self.tcp - self.goal, dim = 1)
+        
         in_place_margin = torch.norm(self.init_tcp - self.goal, dim = 1)
         in_place = tolerance(
             tcp_to_goal,
@@ -28,8 +28,8 @@ class FrankaReachEnv(FrankaBaseEnv):
         return 10*in_place
 
     def _get_dones(self) -> tuple[torch.Tensor, torch.Tensor]:
-        tcp = torch.mean(self.robot.data.body_state_w[:, self.finger_idx, 0:3], dim = 1)
-        tcp_to_goal = torch.norm(tcp - self.goal, dim = 1)
+        self.compute_intermediate()
+        tcp_to_goal = torch.norm(self.tcp - self.goal, dim = 1)
         
         contacts_dones_condition = torch.any(torch.norm(self.sensor.data.net_forces_w[:, self.undesired_contact_body_ids, :], dim=-1) > 1e-3, dim = -1)
         dones = torch.logical_or(contacts_dones_condition, self.target_pos[:,-1]<0.8)
